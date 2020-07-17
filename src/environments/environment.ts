f598faf5659c0a03b2048445b63410df92e682c2
José Panero <panerootero@gmail.com>
2020-07-17 17:28:31 +0200

    Varios cambios realizados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  mapboxKey: 'pk.eyJ1IjoicGFuaTcyIiwiYSI6ImNrY2tvMG0yNDF1cTQydHAwczg3Y3p4NDQifQ.V51SjYxbkJR8_1oV3a8eoQ'
+  mapboxKey: 'SU_API_KEY'
 };
 
 /*
